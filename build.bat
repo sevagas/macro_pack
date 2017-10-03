8aaa9fe983a64d055a9ff2811ee240d9d6107892
emeric.nasi <emeric.nasi@sevagas.com>
2017-10-03 20:42:51 +0200

    Add gitignore and correct builder

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ DEL ".\bin\macro_pack.exe"
 MKDIR "build_tmp" 
 XCOPY "src" "build_tmp\" /E
 CHDIR "build_tmp"
-pyinstaller --clean --onefile -p modules -p pro_modules 
+pyinstaller --clean --onefile -p modules -p pro_modules macro_pack.py
 COPY "dist\macro_pack.exe" "..\bin\macro_pack.exe"
 CHDIR ..
 RMDIR /s /q "build_tmp" 
