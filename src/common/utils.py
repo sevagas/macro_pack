523200a34d942649ad2765f5181c540f5cde7ac2
emeric.nasi <emeric.nasi@sevagas.com>
2018-09-03 09:14:17 +0200

    Correct local ip address display for http and webdav servers

--- src/common/utils.py
+++ src/common/utils.py
@@ -37,7 +37,16 @@ def randomAlpha(length):
 
 def getHostIp():
     """ returne current facing IP address """
-    return(socket.gethostbyname(socket.gethostname()))
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 
 def getRunningApp():
